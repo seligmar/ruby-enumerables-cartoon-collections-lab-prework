3ada797b8fd72585da184f9ae443726d7c1aa3dc
Mary Selig <seligmar@gmail.com>
2019-06-22 09:31:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,8 +3,7 @@ def roll_call_dwarves(dwarves)
 end 
 
 def summon_captain_planet(planateer_calls)
-  memo = planateer_calls.map {|letter| letter.capitalize} 
-  memo.map {|exclaim| "#{exclaim!}"}
+  memo = planateer_calls.map {|letter| "#{letter.capitalize!}"} 
 end 
 
 def long_planeteer_calls# code an argument here
