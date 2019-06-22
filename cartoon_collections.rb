98f648413223d7d67bc0978addd77a9f63bf1113
Mary Selig <seligmar@gmail.com>
2019-06-22 09:33:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ def summon_captain_planet(planateer_calls)
     letter << "!"
   end 
   
-  {|letter| "#{letter.capitalize!}"} 
+#  {|letter| "#{letter.capitalize!}"} 
 end 
 
 def long_planeteer_calls# code an argument here
