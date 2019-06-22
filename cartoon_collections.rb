dc09cde68766d0ec318123cc7c0d871ca9266d30
Mary Selig <seligmar@gmail.com>
2019-06-22 09:46:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,8 @@ def long_planeteer_calls(words)
   end 
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(foods)
+  foods.include?("cheddar" || "gouda" || "camembert")
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
