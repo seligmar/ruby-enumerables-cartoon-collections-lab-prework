25df5b2440535c82faef6e28a76f04a8d32018c1
Mary Selig <seligmar@gmail.com>
2019-06-22 09:54:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,6 +19,5 @@ end
   
 def find_the_cheese(foods)
   if foods.include?("cheddar" || "gouda" || "camembert")
-   foods.find {|cheese| cheese == "cheddar" || cheese == "gouda" 
-                || cheese == "camembert" } 
+   foods.find {|cheese| cheese == "cheddar", "gouda", "camembert" } 
 end
