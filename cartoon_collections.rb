59c2d25edd01228c634180cec6ae60fc5850b4ae
Mary Selig <seligmar@gmail.com>
2019-06-22 08:56:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index {|tyrion, index| puts "#{index}. " + "#{tyrion}"}
+  dwarves.each_with_index {|tyrion, index| puts "#{index +1}. " + "#{tyrion}"}
 end 
 
 def summon_captain_planet# code an argument here
